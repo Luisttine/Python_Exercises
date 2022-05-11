0e88f249310a4c1dff41dbadc231f1e74dbfbbcd
Luis Souza <luisttine@gmail.com>
2022-05-11 19:11:23 -0300

    course finished

--- Atividades SoloLearn/stackBrackets.py
+++ Atividades SoloLearn/stackBrackets.py
@@ -1,40 +1,49 @@
+##CÓDIGO Q TENTEI FAZER - N PASSOU NO ULTIMO TESTE
 def balanced(expression):
     #your code goes here
     stack = []
-    cont = 0
-    for i in expression:
-        if i == "(":
-            stack.insert(0, i)
-        for x in expression:
-            if x == ")":
-                stack.pop()
-    return True if len(stack) == 0 else False
-        
-print(balanced(input()))
-
-def balanced(expression):
-    #your code goes here
-    stack = []
+    cont = 1
     expression = list(expression)
-    cont = 0
     if expression[0] == ")": return False
     for i in expression:
-        cont += 1
-        expression2 = expression[cont:]
         if i == "(":
             stack.insert(0, i)
         else: continue
-        for x in expression2:
+        for x in expression[cont:]:
+            cont += 1
             if x == ")":
                 if stack == False:
                     return False
                 stack.pop()
-                expression.pop()
                 break
-            else: return False
-        cont = 0
+            else: continue
     if stack == False: return True
     return True if len(stack) == 0 else False
         
 print(balanced(input()))
 
+##CÓDIGO CORRETO
+
+def balanced(expression):
+       stack = []
+       opening = set('(')
+       closing = set(')')
+       pair = {')' : '('}
+       for i in expression :
+           if i in opening :
+               stack.append(i)
+           if i in closing :
+               if not stack :
+                   return False
+               elif stack.pop() != pair[i] :
+                   return False
+               else :
+                   continue
+       if not stack :
+           return True
+       else :
+           return False
+        
+print(balanced(input()))
+
+        

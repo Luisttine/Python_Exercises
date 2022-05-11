dc85aa65313b96a2ed350391dd8ba6710f4a1e8a
Luis Souza <luisttine@gmail.com>
2022-05-11 15:39:47 -0300

    final project

---
+++ Atividades SoloLearn/stackBrackets.py
@@ -0,0 +1,40 @@
+def balanced(expression):
+    #your code goes here
+    stack = []
+    cont = 0
+    for i in expression:
+        if i == "(":
+            stack.insert(0, i)
+        for x in expression:
+            if x == ")":
+                stack.pop()
+    return True if len(stack) == 0 else False
+        
+print(balanced(input()))
+
+def balanced(expression):
+    #your code goes here
+    stack = []
+    expression = list(expression)
+    cont = 0
+    if expression[0] == ")": return False
+    for i in expression:
+        cont += 1
+        expression2 = expression[cont:]
+        if i == "(":
+            stack.insert(0, i)
+        else: continue
+        for x in expression2:
+            if x == ")":
+                if stack == False:
+                    return False
+                stack.pop()
+                expression.pop()
+                break
+            else: return False
+        cont = 0
+    if stack == False: return True
+    return True if len(stack) == 0 else False
+        
+print(balanced(input()))
+
